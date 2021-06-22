0d53678248e493b55c8051d7ab7eaa5f6b3029d9
aaronlarouco <aaronlarouco@gmail.com>
2021-06-22 13:25:15 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,5 +5,7 @@ terraform {
     region         = "us-east-1"
     dynamodb_table = "prdinf-terraform-lock-table"
     encrypt        = "true"
+    bucket         = "cb-prod-infra-terraform"
+    key            = "cb-prod-infra/alarouco-test/us-east-2.tfstate"
   }
 }
