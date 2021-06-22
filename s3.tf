08157c096de3d1758ac60a6dfe524f9a7e312bb1
aaronlarouco <aaronlarouco@gmail.com>
2021-06-22 14:31:47 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -7,3 +7,12 @@ resource "aws_s3_bucket" "test" {
     Environment = "test"
   }
 }
+
+resource "aws_s3_bucket" "test2" {
+  bucket = "gitops-pull-test2"
+  
+  tags = {
+    Name        = "gitops-pull-test2"
+    Environment = "test"
+  }
+}
